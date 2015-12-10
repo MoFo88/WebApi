c9c1f8a1b3fcb10c4a46f4644bc059345f234efd
Łukasz Papierz <pope88@gmail.com>
2015-12-10 01:02:46 +0100

    [ADDED] Simple DI to project for Logger and Context

--- WebApi/Controllers/ValuesController.cs
+++ WebApi/Controllers/ValuesController.cs
@@ -1,18 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+using System.IO;
+using System.Web.Hosting;
 using System.Web.Http;
 
 namespace WebApi.Controllers
 {
     public class ValuesController : ApiController
     {
+        private readonly ILogger logger;
+        private readonly IRepository repository;
+
+        public ValuesController(ILogger logger, IRepository repository)
+        {
+            this.logger = logger;
+            this.repository = repository;
+            logger.Log("ValuesController created");
+        }
+
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            logger.Log("Get() invoked");
+            return repository.GetAllValues();
         }
 
         // GET api/values/5
@@ -22,18 +32,60 @@ namespace WebApi.Controllers
         }
 
         // POST api/values
-        public void Post([FromBody]string value)
+        public void Post([FromBody] string value)
         {
         }
 
         // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody] string value)
         {
         }
 
         // DELETE api/values/5
         public void Delete(int id)
         {
+        }
+    }
+
+    public interface IRepository
+    {
+        IEnumerable<string> GetAllValues();
+    }
+
+    public interface ILogger
+    {
+        void Log(string logMessage);
+    }
+
+    public class Logger : ILogger
+    {
+        private string logFile;
+        public Logger()
+        {
+            logFile = HostingEnvironment.MapPath("~/log.txt");
+            File.WriteAllText(logFile, "");
+        }
+
+        public void Log(string logMessage)
+        {
+            File.AppendAllText(logFile,
+                DateTime.Now.ToString("O") + ":: " + logMessage + "\r\n");
+        }
+    }
+
+    public class RepositoryANotImplemented : IRepository
+    {
+        public IEnumerable<string> GetAllValues()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class RepositoryImplemented : IRepository
+    {
+        public IEnumerable<string> GetAllValues()
+        {
+            return new[] {"Values", "returned", "from", "implemented", "and", "injected", "repository"};
         }
     }
 }
